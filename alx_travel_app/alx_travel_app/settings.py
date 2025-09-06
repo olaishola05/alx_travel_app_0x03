ac8ecbfadb681699594da379c447d5b616b640e1
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-09-06 21:03:47 +0100

    remove db_url and used sqlite

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -174,9 +174,9 @@ STORAGES = {
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES ={}
-if 'DATABASE_URL' in os.environ:
-  DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES ={}
+# if 'DATABASE_URL' in os.environ:
+  # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 DATABASES = {
   'default': {
