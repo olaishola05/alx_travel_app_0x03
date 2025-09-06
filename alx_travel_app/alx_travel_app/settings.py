db161d03207100989274d1a38b0f0fb8a9969c56
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-09-06 21:12:00 +0100

    hardcode allowed host

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -34,7 +34,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 DEBUG = env("DEBUG") != 'False'
 SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ['olaish05.pythonanywhere.com','127.0.0.1']
 
 CHAPA_SECRET_KEY = os.environ.get('CHAPA_SECRET_KEY')
 CHAPA_PUBLIC_KEY = os.environ.get('CHAPA_PUBLIC_KEY')
